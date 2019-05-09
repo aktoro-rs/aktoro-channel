fcddef11b97d575900f1b73850c9d1c58197128d
Matthieu Le Brazidec <matthieu@lebrazidec.email>
2019-05-09 10:13:43 +0200

    Added an `unbounded` channel
    - `UnboundedSender` is a wrapper around
      `futures_channel::mpsc::UnboundedSender` that, for now, just stores
      its current state (whether the channel is closed, and whether it
      disconnected itself from it).
    - `UnboundedReceiver` is a wrapper around
      `futures_channel::mpsc::UnboundedReceiver` that, again, for now, just
      tores its current state (just whether the channel is closed).
    - Both implement the same trait as the types they are wrappers of.
    - Both follow the same method naming format as the `once` channel
      (`send`, `try_recv`, `close` and, now, `disconnect`).
    - Both follow the same field naming format as the `once` channel
      (`closed` and, now, `disconnected`).

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,25 @@
+use futures_channel::mpsc;
 use futures_channel::oneshot;
 
 mod error;
 pub mod once;
+pub mod unbounded;
 
 pub use error::*;
 pub use once::*;
+pub use unbounded::*;
 
 pub fn once<D>() -> (OnceSender<D>, OnceReceiver<D>) {
     let (sender, receiver) = oneshot::channel();
 
     (OnceSender::new(sender), OnceReceiver::new(receiver))
+}
+
+pub fn unbounded<D>() -> (UnboundedSender<D>, UnboundedReceiver<D>) {
+    let (sender, receiver) = mpsc::unbounded();
+
+    (
+        UnboundedSender::new(sender),
+        UnboundedReceiver::new(receiver),
+    )
 }
